69d78ef3646b8c88f7a216cd275d387cd519a7ef
Adi-Shuker <adielgali57@gmail.com>
2022-10-24 18:40:09 +0300

    db set up

---
+++ plant.py
@@ -0,0 +1,9 @@
+
+
+class Plant():
+
+    def __init__(self, name, description, image, watering_gaps):
+        self.name = name
+        self.description = description
+        self.image = image
+        self.watering_gaps = watering_gaps
