127057a79fb4f83a10ca08bfc55fcef9057c5ed9
tom-nachum <tom.nachum@gmail.com>
2022-10-25 16:39:37 +0300

    changed access code

--- whatsapp_handler.py
+++ whatsapp_handler.py
@@ -3,7 +3,7 @@ import requests
 
 
 PHONE_NUMBER_ID = "101948529390348"
-ACCESS_TOKEN = "EAAUg6nCLVA0BAIojuyuhREwbeJpXaRc0UA4dVQqz4xpQkmcqqaPzxzPXpVjDUgWXLn0TMvKunUJy1ZBvS8UYlcMCt5QCbrRpTdgAlwZC9UbK02ZCeghUPvitlbxjMaMjwQDnnWDwuTLqRLd2WVWU2dx5AYnoGorZBO9jOIkz8TN8UbBbePnDYIukSZBGXTKRp186mHf1IPoncREzjVYuZCkI5ggTe4qjgZD"
+ACCESS_TOKEN = "EAAUg6nCLVA0BAMgSTeK1FCPCBtPjj19ZCiPuG0bVjMtAckIDeyZBAz1yfw7jPHAbMIGBZAcqjUArI9UqYWw8FyK9mHERA2yMXivwM9W1Ec0dUvhM22SMwwiy9xrSoZB1ZBjRpeNspQCUFvqa5JTIhKGTHGD1QgjS3DRItJkfgdhzBPt4nuwRugXVqJT9T67PHXCD2QZAKipMaodQWBu1wJ8xD4M02qHF4ZD"
 ISRAEL = "972"
 URL = f"https://graph.facebook.com/v14.0/{PHONE_NUMBER_ID}/messages"
 HEADERS = {
@@ -36,3 +36,6 @@ def sendMessage(recipient_phone_number: str, user_name: str, plant_name: str):
     print(response.ok)
     print(response.status_code)
     print(response.content)
+
+
+sendMessage("0504448908", "x", "y")
