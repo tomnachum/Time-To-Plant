db0879bf9081af3fae60c3698cff03a7bfdb9507
MatanNahum <matan.nahum95@gmail.com>
2022-10-25 16:16:50 +0300

    data to send for the whatsapp message

--- insert_data.py
+++ insert_data.py
@@ -2,26 +2,40 @@ from plant import Plant
 import db_manager
 
 
-plants = [Plant('Aglaonema', '', 'aglaonema.jpg', 3),
-          Plant('aloe vera', '', 'aloevera.jpg', 3),
-          Plant('Diepenbachia', '', 'Dieffenbachia_.jpg', 3),
-          Plant('Philodendron', '', 'pilo.jpg', 3),
-          Plant('Calathea', '', '1200px-Calathea_lancifolia_cc5.jpg', 3)]
+plants = [
+    Plant("Aglaonema", "", "aglaonema.jpg", 3),
+    Plant("aloe vera", "", "aloevera.jpg", 3),
+    Plant("Diepenbachia", "", "Dieffenbachia_.jpg", 3),
+    Plant("Philodendron", "", "pilo.jpg", 3),
+    Plant("Calathea", "", "1200px-Calathea_lancifolia_cc5.jpg", 3),
+]
 db_manager.add_plants(plants)
 
-users = [{"name": "Shaked", "email": "shaked@gmail.com",
-          "phone_number": "0541234567"}]
+users = [
+    {"name": "Tom", "email": "tom@gmail.com", "phone_number": "0545400958"},
+    {"name": "Matan", "email": "matan@gmail.com", "phone_number": "0504448908"},
+]
 
 for user in users:
-    db_manager.add_user(user["name"], user['email'], user['phone_number'])
+    db_manager.add_user(user["name"], user["email"], user["phone_number"])
 
-users_plants = [{'user_id': 1, 'plants_id': [1, 2, 3]}]
+users_plants = [
+    {"user_id": 1, "plants_id": [1, 2, 3]},
+    {"user_id": 2, "plants_id": [2, 3]},
+]
 for user_plants in users_plants:
-    db_manager.add_plants_to_user(
-        user_plants['user_id'], user_plants['plants_id'])
+    db_manager.add_plants_to_user(user_plants["user_id"], user_plants["plants_id"])
 
 user_notifications = [
-    {'user_id': 1, 'plant_id': 1, 'time_in_UNIX_TIMESTAMP': 12345678}]
+    {"user_id": 1, "plant_id": 1, "time_in_UNIX_TIMESTAMP": 12345678},
+    {"user_id": 1, "plant_id": 2, "time_in_UNIX_TIMESTAMP": 12345678},
+    {"user_id": 1, "plant_id": 2, "time_in_UNIX_TIMESTAMP": 12345678},
+    {"user_id": 2, "plant_id": 2, "time_in_UNIX_TIMESTAMP": 12345678},
+    {"user_id": 2, "plant_id": 3, "time_in_UNIX_TIMESTAMP": 12345678},
+]
 for notification in user_notifications:
     db_manager.add_notification(
-        notification['user_id'], notification['plant_id'], notification['time_in_UNIX_TIMESTAMP'])
+        notification["user_id"],
+        notification["plant_id"],
+        notification["time_in_UNIX_TIMESTAMP"],
+    )
