69b7d8a9ff8738076bc14807fe4126d84b2ea546
tom-nachum <tom.nachum@gmail.com>
2022-10-25 11:04:34 +0300

    added endpoint to get all plants

--- server.py
+++ server.py
@@ -1,27 +1,39 @@
-phone_number_id = "101948529390348"  # Phone number ID provided
-access_token = "EAAUg6nCLVA0BAAufzYBqHpInRqX0vi7b0dkVIaNTgRKHf0OaZCNJoJJ5u17YoHPiMPHD6XKIylZAROTmYp5xdwT2BQN5rOkd2DqFwRbuUadSEbKTqdfSMs1pWcILdx5SGkHqy9s6C33HrF1dGekh1yDTZA7JJKHoZALCG8fbHTh0kfS3iOMo7vZCJnNFLO2MnIlPJ3N9by6wC5VmnJxL5ZAixfQ7m0kZAkZD"  # Your temporary access token
-recipient_phone_number = "972545400958"  # Your own phone number
-
-url = f"https://graph.facebook.com/v14.0/{phone_number_id}/messages"
-headers = {
-    "Authorization": f"Bearer {access_token}",
-    "Content-Type": "application/json",
-}
-
-data = {
-    "messaging_product": "whatsapp",
-    "to": recipient_phone_number,
-    "type": "template",
-    "template": {
-        "name": "timetoplant",
-        "language": {"code": "en_US"},
-    },
-}
-
-import json
+from fastapi import FastAPI, Request, Response
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 import requests
 
+from db_manager import get_all_plants
 
-response = requests.post(url, headers=headers, data=json.dumps(data))
-print(response.ok)
-print(response.status_code)
+app = FastAPI()
+
+# JS_FILES_DIR = "client/build"
+# TEMPLATE_DIR = "client/src"
+# HTML_DIR = "client\src\index.html"
+
+# app.mount(
+#     f"/{JS_FILES_DIR}",
+#     StaticFiles(directory=JS_FILES_DIR),
+#     name=JS_FILES_DIR,
+# )
+
+# app.mount(
+#     f"/{TEMPLATE_DIR}",
+#     StaticFiles(directory=TEMPLATE_DIR),
+#     name=TEMPLATE_DIR,
+# )
+
+
+# @app.get("/")
+# def get_html():
+#     return FileResponse(HTML_DIR)
+
+
+@app.get("/plants")
+def get_plants():
+    return get_all_plants()
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
