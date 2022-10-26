290981b45091e9e09d312102ee7bb91ae253c1a1
MatanNahum <matan.nahum95@gmail.com>
2022-10-26 14:43:26 +0300

    schedule improvment

--- scheduler.py
+++ scheduler.py
@@ -1,13 +1,42 @@
 from db_manager import get_data_for_whatsapp
 from whatsapp_handler import sendMessage
 import time
+import sched
+import schedule
 
-while True:
+# while True:
+#     data_to_send_on_whatsapp_array = get_data_for_whatsapp()
+#     for dataUnit in data_to_send_on_whatsapp_array:
+#         sendMessage(
+#             dataUnit["phone_number"], dataUnit["user_name"], dataUnit["plant_name"]
+#         )
+
+#     time.sleep(30)
+#     print("iteration")
+
+
+def sendWhatsappAlert():
     data_to_send_on_whatsapp_array = get_data_for_whatsapp()
     for dataUnit in data_to_send_on_whatsapp_array:
-        sendMessage(
-            dataUnit["phone_number"], dataUnit["user_name"], dataUnit["plant_name"]
-        )
+        # sendMessage(
+        #     dataUnit["phone_number"], dataUnit["user_name"], dataUnit["plant_name"]
+        # )
+        print(dataUnit)
+    return
 
-    time.sleep(30)
-    print("iteration")
+
+# def job():
+#     print("I'm working...")
+
+
+schedule.every(10).seconds.do(sendWhatsappAlert)
+# schedule.every(10).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every(5).to(10).minutes.do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
