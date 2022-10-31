567ae96346bdc54f6ff05a675ae3d7b06e94bf9d
tom-nachum <tom.nachum@gmail.com>
2022-10-31 19:43:55 +0200

    updated scheduler

--- client/plantsController.js
+++ client/plantsController.js
@@ -2,34 +2,34 @@ const renderer = new Renderer();
 const plants = new Plants();
 $(".profile").removeClass("active");
 $(".home").addClass("active");
-plants.getPlants().then((plants) => {
-    renderer.render(plants);
+plants.getPlants().then(plants => {
+  renderer.render(plants);
 });
-plants.getPlantsNames().then((plants_names) => {
-    const autoCompleteJS = new autoComplete({
-        placeHolder: "Search for Plant...",
-        data: {
-            src: plants_names,
-            cache: true,
-        },
-        resultItem: {
-            highlight: true,
-        },
-        events: {
-            input: {
-                selection: (event) => {
-                    const selection = event.detail.selection.value;
-                    autoCompleteJS.input.value = selection;
-                    plants.getPlantByName(selection).then((plants) => {
-                        renderer.render(plants);
-                    });
-                },
-            },
-        },
-    });
-});
+// plants.getPlantsNames().then(plants_names => {
+//   const autoCompleteJS = new autoComplete({
+//     placeHolder: "Search for Plant...",
+//     data: {
+//       src: plants_names,
+//       cache: true,
+//     },
+//     resultItem: {
+//       highlight: true,
+//     },
+//     events: {
+//       input: {
+//         selection: event => {
+//           const selection = event.detail.selection.value;
+//           autoCompleteJS.input.value = selection;
+//           plants.getPlantByName(selection).then(plants => {
+//             renderer.render(plants);
+//           });
+//         },
+//       },
+//     },
+//   });
+// });
 $(".plants").on("click", ".add-reminder", function () {
-    userId = $(this).data().userId;
-    plantId = $(this).data().plantId;
-    plants.addPlantToUser(userId, plantId);
+  userId = $(this).data().userId;
+  plantId = $(this).data().plantId;
+  plants.addPlantToUser(userId, plantId);
 });
--- scheduler.py
+++ scheduler.py
@@ -1,18 +1,7 @@
 from db_manager import get_data_for_whatsapp
 from whatsapp_handler import sendMessage
 import time
-import sched
 import schedule
-
-# while True:
-#     data_to_send_on_whatsapp_array = get_data_for_whatsapp()
-#     for dataUnit in data_to_send_on_whatsapp_array:
-#         sendMessage(
-#             dataUnit["phone_number"], dataUnit["user_name"], dataUnit["plant_name"]
-#         )
-
-#     time.sleep(30)
-#     print("iteration")
 
 
 def sendWhatsappAlert():
@@ -27,19 +16,8 @@ def sendWhatsappAlert():
     return
 
 
-# def job():
-#     print("I'm working...")
-
-
 schedule.every(30).seconds.do(sendWhatsappAlert)
-# schedule.every(10).minutes.do(job)
-# schedule.every().hour.do(job)
-# schedule.every().day.at("10:30").do(job)
-# schedule.every(5).to(10).minutes.do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
 
-time.sleep(5)
 while True:
     schedule.run_pending()
     time.sleep(1)
